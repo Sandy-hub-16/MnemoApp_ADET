a9ad8abdf656b36f9733d26e45ee180fce35e78d
Sandy <napiza.sandy.lsei@gmail.com>
2026-06-12 11:46:42 +0800

    migrate to dart-define, remove flutter_dotenv

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,4 @@
 const nodemailer = require("nodemailer");
-require("dotenv").config();
 
 const { onSchedule } = require("firebase-functions/v2/scheduler");
 const admin = require("firebase-admin");
